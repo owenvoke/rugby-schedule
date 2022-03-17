b4a2f3be786d70273201b3fa0a6030143e213dd4
Owen Voke <development@voke.dev>
2022-03-17 15:28:35 +0000

    feat: add Premiership Rugby command

---
+++ app/Commands/PremiershipCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Commands;
+
+use App\Contracts\ScheduleCommand;
+
+class PremiershipCommand extends ScheduleCommand
+{
+    /** {@inheritdoc} */
+    protected $signature = 'premiership {team? : An optional team name} {--include-past : Include past events}';
+
+    /** {@inheritdoc} */
+    protected $description = 'View the Premiership schedule';
+
+    protected function getFeedUrl(): string
+    {
+        return sprintf(
+            'https://cdn.soticservers.net/tools/wordpress/ical/calendar.php?CompId=4304&source=sfms&project=premier&TeamId=%s',
+            $this->getTeamFromOption()
+        );
+    }
+
+    private function getTeamFromOption(): string
+    {
+        return match (strtolower($this->argument('team'))) {
+            '' => '',
+            'bath' => '1',
+            'gloucester' => '2',
+            'leicester', 'tigers' => '4',
+            'london-irish', 'london irish' => '5',
+            'wasps' => '6',
+            'harlequins', 'quins' => '7',
+            'newcastle', 'falcons' => '8',
+            'northampton', 'saints' => '9',
+            'saracens' => '11',
+            'sale', 'sharks' => '12',
+            'bristol', 'bears' => '25',
+            'exeter', 'chiefs' => '48',
+            'worcester', 'warriors' => '57',
+        };
+    }
+}
