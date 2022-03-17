c6966d5534ba70d0aa5ed1d728e17fe7b7ada5df
Owen Voke <development@voke.dev>
2022-03-17 17:32:28 +0000

    fix: silence PHPStan warnings

--- app/helpers.php
+++ app/helpers.php
@@ -3,15 +3,15 @@
 use Sabre\VObject\Component\VEvent;
 
 if (! function_exists('google_calendar_event')) {
-    function google_calendar_event(VEvent $event)
+    function google_calendar_event(VEvent $event): string
     {
         return sprintf(
             'https://calendar.google.com/calendar/r/eventedit?text=%s&dates=%s/%s&details=%s&location=%s',
-            urlencode(str($event->SUMMARY->getValue())->before(',')),
-            urlencode($event->DTSTART->getValue()),
-            urlencode($event->DTEND->getValue()),
-            urlencode(str($event->SUMMARY->getValue())->before(',').PHP_EOL.PHP_EOL.$event->URL->getValue()),
-            urlencode($event->LOCATION->getValue()),
+            urlencode(str($event->SUMMARY->getValue())->before(',')), // @phpstan-ignore-line
+            urlencode($event->DTSTART->getValue()), // @phpstan-ignore-line
+            urlencode($event->DTEND->getValue()), // @phpstan-ignore-line
+            urlencode(str($event->SUMMARY->getValue())->before(',').PHP_EOL.PHP_EOL.$event->URL->getValue()), // @phpstan-ignore-line
+            urlencode($event->LOCATION->getValue()), // @phpstan-ignore-line
         );
     }
 }
