45a0c2d59d3b35464bc7a538b988f6c943fbfb5c
Owen Voke <development@voke.dev>
2022-03-18 15:36:26 +0000

    feat: add Women's Six Nations command

---
+++ app/Commands/WomensSixNationsCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Commands;
+
+use App\Contracts\ScheduleCommand;
+
+class WomensSixNationsCommand extends ScheduleCommand
+{
+    /** {@inheritdoc} */
+    protected $signature = 'womens:six-nations {team? : An optional team name}
+                                        {--p|include-past : Include past events}
+                                        {--c|include-calendar-links : Include calendar links}';
+
+    protected function getFeedName(): string
+    {
+        return 'Women\'s Six Nations';
+    }
+
+    protected function getFeedUrl(): string
+    {
+        return sprintf(
+            'https://cdn.soticservers.net/tools/wordpress/ical/calendar.php?CompId=4339&source=sfms&project=sixnations&TeamId=%s',
+            $this->getTeamFromOption()
+        );
+    }
+
+    private function getTeamFromOption(): string
+    {
+        return match (strtolower($this->argument('team'))) {
+            'england'  => '1790',
+            'ireland'  => '1791',
+            'scotland' => '1792',
+            'france'   => '1805',
+            'italy'    => '1839',
+            'wales'    => '3007',
+            default    => '',
+        };
+    }
+}
